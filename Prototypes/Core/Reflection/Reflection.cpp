84d7c07302407ac2071ab12b9f09cfb564dce3b2
Eaglegor <eaglegor@gmail.com>
2015-10-14 00:19:58 +0400

    More reflection experiments

--- Prototypes/Core/Reflection/Reflection.cpp
+++ Prototypes/Core/Reflection/Reflection.cpp
@@ -1,10 +1,27 @@
 #include <unordered_map>
 #include <iostream>
+#include <chrono>
 
 class Methods {
 };
 
-class Properties {
+class Field;
+class Fields {
+public:
+	void addField(const char* name, Field* field)
+	{
+		fields.emplace(name, field);
+	}
+
+	const Field* getField(const char* name) const
+	{
+		auto iter = fields.find(name);
+		if (iter == fields.end()) return nullptr;
+		return iter->second;
+	}
+
+private:
+	std::unordered_map<std::string, Field*> fields;
 };
 
 class Constructors {
@@ -35,7 +52,7 @@ class IMetaType {
 
 		virtual const Methods &getMethods() const = 0;
 
-		virtual const Properties &getProperties() const = 0;
+		virtual const Fields &getFields() const = 0;
 
 		virtual const IMetaType *getBaseType() const = 0;
 
@@ -49,26 +66,6 @@ class IMetaType {
 			return !(*this == rhs);
 		}
 };
-
-
-class TypeIdManager {
-	public:
-		size_t getTypeId(const char *name) {
-			auto iter = mappings.find(name);
-			if (iter == mappings.end()) {
-				int val = counter++;
-				mappings.emplace(name, val);
-				return val;
-			}
-			return iter->second;
-		}
-
-	private:
-		size_t counter = 0;
-		std::unordered_map<std::string, size_t> mappings;
-};
-
-TypeIdManager type_id_manager;
 
 class A {
 	public:
@@ -98,6 +95,32 @@ class MetaType : public IMetaType {
 		MetaType() = delete;
 };
 
+class TypeIdManager {
+public:
+	size_t getTypeId(const char *name) {
+		auto iter = mappings.find(name);
+		if (iter == mappings.end()) {
+			int val = counter++;
+			mappings.emplace(name, val);
+			return val;
+		}
+		return iter->second;
+	}
+
+	template<typename T>
+	MetaType<T>& getMetaType()
+	{
+		static MetaType<T> meta_type;
+		return meta_type;
+	}
+
+private:
+	size_t counter = 0;
+	std::unordered_map<std::string, size_t> mappings;
+};
+
+TypeIdManager type_id_manager;
+
 class ObjectRefHolder {
 	public:
 
@@ -106,7 +129,7 @@ class ObjectRefHolder {
 
 		template<typename T>
 		T *cast() {
-			MetaType<T> type;
+			MetaType<T>& type = type_id_manager.getMetaType<T>();
 			if (type != *getType()) {
 				std::cout << "Bad type" << std::endl;
 				return nullptr;
@@ -114,26 +137,46 @@ class ObjectRefHolder {
 			return static_cast < T * >(getPointer());
 		}
 
+		template<typename T>
+		void set(T* ptr)
+		{
+			MetaType<T>& type = type_id_manager.getMetaType<T>();
+			if(type != *getType())
+			{
+				std::cout << "Bad type" << std::endl;
+				return;
+			}
+			setPointer(ptr);
+		}
+
 	protected:
 		ObjectRefHolder() {
 		}
 
 	private:
-		virtual IMetaType *getType() const = 0;
+		virtual const IMetaType *getType() const = 0;
 
 		virtual void *getPointer() = 0;
+		virtual void setPointer(void* ptr) = 0;
 };
 
 template<typename T>
 class TypedObjectHolder : public ObjectRefHolder {
 	public:
+		TypedObjectHolder():
+			pointer(nullptr),
+			type(type_id_manager.getMetaType<T>())
+		{
+		}
+
 		TypedObjectHolder(T *ptr) :
 				pointer(ptr),
-				type(new MetaType<T>()) {
+				type(type_id_manager.getMetaType<T>())
+		{
 		}
 
 		~TypedObjectHolder() {
-			delete type;
+			//delete type;
 		}
 
 	private:
@@ -141,27 +184,129 @@ class TypedObjectHolder : public ObjectRefHolder {
 			return pointer;
 		}
 
-		virtual IMetaType *getType() const override {
-			return type;
+		virtual void setPointer(void* ptr) override {
+			pointer = static_cast<T*>(ptr);
 		}
 
-		MetaType<T> *type;
-		T *pointer;
+		virtual const IMetaType *getType() const override {
+			return &type;
+		}
 
+		MetaType<T> &type;
+		T *pointer;
 };
 
-class Property {
+class Field {
 	public:
+		virtual ~Field(){};
+
+		template<typename T, typename Cls>
+		T& ref(Cls *cls) const
+		{
+			return *static_cast<T*>(getPointer(cls));
+		}
+
 		template<typename Cls, typename T>
-		void setValue(Cls *cls, const T &value) {
-			TypedObjectHolder<Cls> objholder(cls);
-			TypedObjectHolder<T> valueHolder(value);
+		void setValue(Cls *cls, const T &value) const {
+			setValueImpl(cls, &value);
+		}
+
+		template<typename Cls, typename T>
+		const T& getValue(Cls *cls) const
+		{
+			void* value;
+			getValueImpl(cls, &value);
+			return *reinterpret_cast<T*>(value);
 		}
 
 		virtual IMetaType *getType() = 0;
+		//virtual IMetaType *getClassType() = 0;
+		virtual const char* getName() = 0;
+
+	protected:
+		Field(){}
 
 	private:
-		virtual void setValueImpl(ObjectRefHolder &object_holder, ObjectRefHolder &value_holder) = 0;
+		virtual void setValueImpl(void* cls, const void* value) const = 0;
+		virtual void getValueImpl(void* cls, void** value) const = 0;
+		virtual void* getPointer(void *cls) const = 0;
+};
+
+template<typename Cls, typename T>
+class TypedField : public Field
+{
+public:
+	TypedField(const char* name, T Cls::*ptr):
+		name(name),
+		field_ptr(ptr),
+		type(type_id_manager.getMetaType<T>())
+		//cls_type(type_id_manager.getMetaType<Cls>())
+	{
+	}
+
+	IMetaType* getType() override
+	{
+		return &type;
+	}
+
+	/*IMetaType* getClassType() override
+	{
+		return &cls_type;
+	}*/
+
+	const char* getName() override
+	{
+		return name.c_str();
+	}
+
+private:
+	void setValueImpl(void* cls, const void* value) const override
+	{
+		Cls* clsptr = static_cast<Cls*>(cls);
+		const T* val = static_cast<const T*>(value);
+		clsptr->*field_ptr = *val;
+	}
+
+	void getValueImpl(void* cls, void** value) const override
+	{
+		Cls* clsptr = static_cast<Cls*>(cls);
+		*value = &(clsptr->*field_ptr);
+	}
+
+	void* getPointer(void* cls) const override
+	{
+		Cls* clsptr = static_cast<Cls*>(cls);
+		return &(clsptr->*field_ptr);
+	}
+
+	MetaType<T> &type;
+	//MetaType<Cls> &cls_type;
+	T Cls::*field_ptr;
+	std::string name;
+};
+
+template<typename Cls, typename T>
+class TypedBoundField
+{
+public:
+	TypedBoundField(const Field* field, Cls* cls):
+		t(field->ref<T>(cls))
+	{
+	}
+	
+	TypedBoundField& operator=(const T& rhs)
+	{
+		t = rhs;
+		return *this;
+	}
+
+	void setValue(const T& val)
+	{
+		t = val;
+	}
+
+private:
+	T& t;
 };
 
 template<typename T>
@@ -198,8 +343,8 @@ class BaseMetaType : public IMetaType {
 			return methods;
 		}
 
-		virtual const Properties &getProperties() const override {
-			return properties;
+		virtual const Fields &getFields() const override {
+			return fields;
 		}
 
 		virtual const IMetaType *getBaseType() const override {
@@ -219,13 +364,18 @@ class BaseMetaType : public IMetaType {
 			this->type = type;
 		}
 
+		void addField(const char* name, Field* field)
+		{
+			fields.addField(name, field);
+		}
+
 	private:
 		size_t type_id;
 		std::string type_name;
 		size_t size;
 		ClassType type;
 		Methods methods;
-		Properties properties;
+		Fields fields;
 		Constructors constructors;
 		Destructor destructor;
 };
@@ -236,7 +386,7 @@ class BaseDerivedMetaType : public BaseMetaType<T> {
 
 	public:
 		virtual const IMetaType *getBaseType() const override {
-			return base_class_meta_type;
+			return &base_class_meta_type;
 		}
 
 		virtual bool isDerivedFrom(IMetaType *base_type) const override {
@@ -245,16 +395,14 @@ class BaseDerivedMetaType : public BaseMetaType<T> {
 
 	protected:
 		BaseDerivedMetaType(const char *name, ClassType type) :
-				BaseMetaType<T>(name, type) {
-			base_class_meta_type = new MetaType<BaseClass>();
-		}
-
-		~BaseDerivedMetaType() {
-			delete base_class_meta_type;
+				BaseMetaType<T>(name, type),
+			base_class_meta_type(type_id_manager.getMetaType<BaseClass>())
+		{
+			
 		}
 
 	private:
-		MetaType<BaseClass> *base_class_meta_type;
+		MetaType<BaseClass> &base_class_meta_type;
 };
 
 template<>
@@ -277,16 +425,28 @@ template<>
 class MetaType<A> : public BaseMetaType<A> {
 	public:
 		MetaType() :
-				BaseMetaType("A", ClassType::CLASS) {
+				BaseMetaType("A", ClassType::CLASS),
+				intProperty("intProperty", &A::intProperty)
+		{
+			addField("intProperty", &intProperty);
 		}
+
+	private:
+		TypedField<A, int> intProperty;
 };
 
 template<>
 class MetaType<B> : public BaseMetaType<B> {
 	public:
 		MetaType() :
-				BaseMetaType("B", ClassType::CLASS) {
+				BaseMetaType("B", ClassType::CLASS),
+				floatProperty("floatProperty", &B::floatProperty)
+		{
+			addField("floatProperty", &floatProperty);
 		}
+
+	private:
+		TypedField<B, float> floatProperty;
 };
 
 template<>
@@ -315,8 +475,52 @@ int main() {
 	MetaType<float> floatMetaType2;
 	std::cout << floatMetaType2.getTypeName() << " = " << floatMetaType2.getTypeId() << std::endl;
 
+	A a;
+
+	std::chrono::time_point<std::chrono::system_clock> start, end;
+	
+	A* pa = &a;
+
+	start = std::chrono::system_clock::now();
+
+	for (int i = 0; i < 100000000; ++i)
+	{
+		pa->intProperty = i;
+	}
+
+	end = std::chrono::system_clock::now();
+
+	std::cout << "Plain: " << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() << std::endl;
+	
+	MetaType<A> &amt = type_id_manager.getMetaType<A>();
+	const Field *f = amt.getFields().getField("intProperty");
+
+	TypedBoundField<A, int> bound_field(f, &a);
+
+	int &ia = f->ref<int, A>(&a); 
+
+	start = std::chrono::system_clock::now();
+
+	for (int i = 0; i < 100000000; ++i)
+	{
+		bound_field.setValue(i);
+	}
+
+	end = std::chrono::system_clock::now();
+
+	std::cout << "Reflection: " << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() << std::endl;
+	
+	std::cout << f->getValue<A, int>(&a) << std::endl;
+
+	B b;
+
 	MetaType<B> bmt;
 	std::cout << bmt.getTypeName() << " = " << bmt.getTypeId() << std::endl;
+
+	const Field *ff = bmt.getFields().getField("floatProperty");
+	ff->setValue(&b, 3.14f);
+
+	std::cout << ff->getValue<B, float>(&b) << std::endl;
 
 	MetaType<C> bmt2;
 	std::cout << bmt2.getTypeName() << " = " << bmt2.getTypeId() << " derived from " << bmt2.
@@ -332,6 +536,8 @@ int main() {
 	}
 	while (dmtp != nullptr);
 	std::cout << "END" << std::endl;
+
+	getchar();
 
 	return 0;
 }
